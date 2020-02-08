25f1324320a8d495468d6ce2b55e1cc4cd2b8980
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-02-08 22:14:28 +0900

    Add fade in

--- src/Components/Tweets.jsx
+++ src/Components/Tweets.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom'
+import Fade from 'react-reveal/Fade';
 // import PropTypes from 'prop-types';
 
 export default class Tweets extends Component {
@@ -19,27 +20,29 @@ export default class Tweets extends Component {
         <Container fluid style={{ marginTop: '40px' }}>
           <ul style={{ listStyle: 'none', paddingInlineStart: '0' }}>
             <Row style={{ justifyContent: 'center' }}>
-              {tweets.map(tweet => (
-                <li key={`tweet-${tweet.id}`}>
-                  <Col xs={6} md={4} lg={3}>
-                    <Card style={{ width: '300px', height: '400px', marginBottom: '30px', backgroundColor: 'transparent', border: '0.5px solid white' }}>
-                      <Card.Img variant="top" src={`http://localhost:3001${tweet.image}`} style={{ height: '200px', width: 'auto', maxWidth: '98%', margin: '5px auto' }} />
-                      <Card.Body>
-                        <Card.Title>{tweet.title}</Card.Title>
-                        <Card.Text style={{ height: '65px', overflow: 'hidden' }}>
-                          {tweet.content}
-                        </Card.Text>
-                        <Link to={`/tweets/${tweet.id}`}>
-                          <Button variant="outline-light" >詳細ページ</Button>
-                        </Link>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                </li>
-              ))}
+              <Fade big cascade>
+                {tweets.map(tweet => (
+                  <li key={`tweet-${tweet.id}`}>
+                    <Col xs={6} md={4} lg={3}>
+                      <Card style={{ width: '300px', height: '400px', marginBottom: '30px', backgroundColor: 'transparent', border: '0.5px solid white' }}>
+                        <Card.Img variant="top" src={`http://localhost:3001${tweet.image}`} style={{ height: '200px', width: 'auto', maxWidth: '98%', margin: '5px auto' }} />
+                        <Card.Body>
+                          <Card.Title>{tweet.title}</Card.Title>
+                          <Card.Text style={{ height: '65px', overflow: 'hidden' }}>
+                            {tweet.content}
+                          </Card.Text>
+                          <Link to={`/tweets/${tweet.id}`}>
+                            <Button variant="outline-light" >詳細ページ</Button>
+                          </Link>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  </li>
+                ))}
+              </Fade>
             </Row>
           </ul>
-        </Container>
+        </Container >
       );
     }
   }
